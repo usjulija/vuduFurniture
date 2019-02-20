6b22d9c6aec13c122989cfe284441472b916f175
Julia <us.julija@gmail.com>
2019-02-20 14:31:20 +0200

    price formated with regexp

--- js/product.js
+++ js/product.js
@@ -4,11 +4,24 @@ function responsiveNav() {
   topnav.className === "topnav" ? topnav.className += " responsive" : topnav.className = "topnav";
 }
 
+// const formatPrice = new Intl.NumberFormat("FR", {
+//   style: "currency",
+//   currency: "EUR"
+// });
+
+// function formatPrice(cents) {
+//   return (cents / 100).toLocaleString("FR", {
+//     style: "currency",
+//     currency: "EUR"
+//   });
+// }
+
 function formatPrice(cents) {
-  return (cents / 100).toLocaleString("FR", {
-    style: "currency",
-    currency: "EUR"
-  });
+  return (
+    (cents / 100).toFixed(2) //allows two decimals
+    .replace('.', ',') //replace decimal point character with ,
+    .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1 ') + ' €'
+  );
 }
 
 //product card generator
